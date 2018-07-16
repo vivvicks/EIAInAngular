f6f101d1a0a982c1fb9b3e1c9c3410fb171db969
vivvicks <chavan.viv@gmail.com>
2018-07-16 15:07:59 +0530

    Changes on 16072018

--- src/app/login/loginform/loginform.component.ts
+++ src/app/login/loginform/loginform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {Md5} from 'ts-md5/dist/md5';
+import { Md5 } from 'ts-md5/dist/md5';
+
 
 @Component({
   selector: 'app-loginform',
@@ -18,8 +19,18 @@ export class LoginformComponent implements OnInit {
     e.preventDefault();
     var username = e.target.elements[0].value;
     var password = e.target.elements[1].value;
-    console.log(hex_md5(password).toUpperCase() + username);
-    console.log(Math.random());
+    console.log(this.getRandomInt(0,990));
+    const md5 = new Md5();
+    var md5pwd = md5.appendStr(password).end()
+    var md5pwdUsername =  md5pwd.toString().toUpperCase() + username;
+    var md5pwdUsernamenew = md5.appendStr(md5pwdUsername).end();
+    var md5pwdUsernamenewKey = md5pwdUsernamenew.toString().toUpperCase() + this.getRandomInt(0,990)
+    console.log(md5pwdUsernamenewKey); 
+    var Finalmd5pwdUsernamenewKey = md5.appendStr(md5pwdUsernamenewKey).end();
+    console.log(Finalmd5pwdUsernamenewKey);
   }
 
+   getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
