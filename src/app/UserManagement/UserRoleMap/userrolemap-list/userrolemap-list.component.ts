19cc976d4f6b3a20b383899916516b29107259c2
vivvicks <chavan.viv@gmail.com>
2018-11-16 17:25:10 +0530

    updated on 16112018

--- src/app/UserManagement/UserRoleMap/userrolemap-list/userrolemap-list.component.ts
+++ src/app/UserManagement/UserRoleMap/userrolemap-list/userrolemap-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RepositoryService } from '../../../shared/services/repository.service';
 import { ErrorHandlerService } from '../../../shared/services/error-handler.service';
 import { VsecRoleMst } from '../../../_interfaces/UserManagement/vsecRoleMst.modal';
-import { UserRoleMap } from '../../../_interfaces/UserManagement/userRoleMap.modal';
+import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DropDownList } from '../../../_interfaces/dropdownlist.model';
 
 @Component({
   selector: 'app-userrolemap-list',
@@ -13,24 +15,75 @@ export class UserrolemapListComponent implements OnInit {
 
   public errorMessage = '';
   vsecRoleMst: VsecRoleMst[];
-  UserRoleMapModal: UserRoleMap = new UserRoleMap();
+  // UserRoleMapModal: UserRoleMap = new UserRoleMap();
+  selectedOption: VsecRoleMst;
+  formUserRoleMapGroup: FormGroup = null;
+
+  public statusLst: DropDownList[] = [
+    new DropDownList(1, 'Available'),
+    new DropDownList(2, 'Allocated')
+  ];
+
+  formErrors = {
+    'customSelectedControl': '',
+    'statusSelectedControl': ''
+  };
+
+
+  validationMessages = {
+    'customSelectedControl': {
+      'required': 'customSelectedControl is required.'
+    },
+    'statusSelectedControl': {
+      'required': 'statusSelectedControl is required.'
+    }
+  };
 
   constructor(private repository: RepositoryService,
-              private errorHandler: ErrorHandlerService) { }
+    private errorHandler: ErrorHandlerService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
     this.GetAllRoles();
   }
 
   public GetAllRoles() {
     this.repository.getData('api/RoleCreation/GetAllRoles')
-          .subscribe(response => {
-           this.vsecRoleMst = response as VsecRoleMst[];
-          }, err => {
+      .subscribe(response => {
+        this.vsecRoleMst = response as VsecRoleMst[];
+      }, err => {
         this.errorHandler.handleError(err);
         this.errorMessage = this.errorHandler.errorMessage;
       });
   }
 
+  onSelect(event: TypeaheadMatch): void {
+    this.selectedOption = event.item;
+  }
 
+  buildForm(): void {
+    this.formUserRoleMapGroup = this.fb.group({
+      'customSelectedControl': ['', Validators.required],
+      'statusSelectedControl': ['', Validators.required]
+    });
+
+    this.formUserRoleMapGroup.valueChanges.subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.formUserRoleMapGroup) { return; }
+    const form = this.formUserRoleMapGroup;
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
 }
--- src/app/_interfaces/UserManagement/userRoleMap.modal.ts
+++ src/app/_interfaces/UserManagement/userRoleMap.modal.ts
@@ -1,13 +1,62 @@
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DropDownList } from '../dropdownlist.model';
 
 export class UserRoleMap {
     customSelected: string;
     formUserRoleMapGroup: FormGroup = null;
+    fb: FormBuilder;
+
+    public statusLst: DropDownList[] = [
+        new DropDownList(1, 'Available'),
+        new DropDownList(2, 'Allocated')
+    ];
 
     constructor() {
-        var _builder = new FormBuilder();
-        this.formUserRoleMapGroup = _builder.group({});
+        // var _builder = new FormBuilder();
+        // this.formUserRoleMapGroup = _builder.group({});
 
-        this.formUserRoleMapGroup.addControl('customSelectedControl',  new FormControl('', Validators.required));
+        // this.formUserRoleMapGroup.addControl('customSelectedControl',  new FormControl('', Validators.required));
+        // this.formUserRoleMapGroup.addControl('statusSelectedControl',  new FormControl(null));
+    }
+
+    formErrors = {
+        'customSelectedControl': '',
+        'statusSelectedControl': ''
+    };
+
+
+    validationMessages = {
+        'customSelectedControl': {
+            'required': 'customSelectedControl is required.'
+        },
+        'statusSelectedControl': {
+            'required': 'statusSelectedControl is required.'
+        }
+    };
+
+    buildForm(): void {
+        this.formUserRoleMapGroup = this.fb.group({
+            'customSelectedControl': ['customSelectedControl', Validators.required],
+            'statusSelectedControl': ['statusSelectedControl', Validators.required]
+        });
+
+        this.formUserRoleMapGroup.valueChanges.subscribe(data => this.onValueChanged(data));
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.formUserRoleMapGroup) { return; }
+        const form = this.formUserRoleMapGroup;
+
+        for (const field in this.formErrors) {
+            this.formErrors[field] = '';
+            const control = form.get(field);
+
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                    this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
     }
 }
